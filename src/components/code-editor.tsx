a7499bf50ff2c7a6f991673ab4c352794c02ba06
Adeola23 <amosadeola92@gmail.com>
2022-07-25 17:18:00 +0100

    implemeted automatic code formatting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,16 +1,64 @@
-import MonacoEditor from '@monaco-editor/react';
+import Editor, { useMonaco } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import { useRef } from 'react';
 
-const CodeEditor = () => {
-  return <MonacoEditor theme="vs-dark" language="javascript" height="500px" options={{
-      wordWrap: 'on',
-      minimap: {enabled: false},
-      showUnused: false,
-      folding: false,
-      lineNumbersMinChars: 3,
-      fontSize: 16,
-      scrollBeyondLastLine: false,
-      automaticLayout: true
-  }} />;
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const monacoRef = useRef<any>();
+  const editorRef = useRef<any>();
+  function handleEditorChange(value: any, event: any) {
+    console.log('here is the current model value:', value);
+
+    onChange(value);
+  }
+  function handleEditorDidMount(editor: any, monaco: any) {
+    // here is another way to get monaco instance
+    // you can also store it in `useRef` for further usage
+    monacoRef.current = editor;
+  }
+
+  const onFormatClick = () => {
+    const unformatted = monacoRef.current.getModel().getValue();
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    monacoRef.current.setValue(formatted)
+    console.log(monacoRef.current);
+  };
+
+  return (
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <Editor
+        theme="vs-dark"
+        language="javascript"
+        height="500px"
+        value={initialValue}
+        onChange={handleEditorChange}
+        onMount={handleEditorDidMount}
+        options={{
+          wordWrap: 'on',
+          tabSize: 2,
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
 };
 
 export default CodeEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -73,7 +73,10 @@ const App = () => {
     `;
   return (
     <div>
-      <CodeEditor/>
+      <CodeEditor
+        initialValue="const a = 1"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -83,7 +86,12 @@ const App = () => {
       </div>
 
       <div>
-        <iframe title="code preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+        <iframe
+          title="code preview"
+          ref={iframe}
+          sandbox="allow-scripts"
+          srcDoc={html}
+        />
       </div>
     </div>
   );
